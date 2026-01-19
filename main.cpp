57eff6bee484b4d31bd9d44e507f1f2bb054276a
mochineko <mochinekoo0113@gmail.com>
2026-01-19 16:13:34 +0900

    [add]　ダイアログが出るようになりました

--- main.cpp
+++ main.cpp
@@ -13,6 +13,21 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	}
 }
 
+INT_PTR CALLBACK DlglogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message) {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
     case WM_COMMAND: {
@@ -26,6 +41,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 DestroyWindow(hWnd);
                 break;
             }
+            case ID_40003: {
+                DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG1), hWnd, DlglogProc);
+                break;
+			}
         }
         break;
     }
